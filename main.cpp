c009a41ab511c1580549cfc28224baf2bd9a8c3b
Jacob <jacobkwiat@gmail.com>
2026-03-25 14:34:16 -0700

    Main project file upload from local computer

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+enum CrdCard{
+    AmEx, Visa, MasterCard, Discover, ALL
+};
+
+void genCC(CrdCard, char[]);
+void flipDig(char[]);
+bool validCC(char[]);
+void Luhn(char ccc[]);
+int main()
+{
+    CrdCard type = Discover;
+    int countP = 0;
+    int countN = 0;
+    char tester[17];
+    srand(time(0));
+    for(int i = 0; i < 10000; i++) {
+        genCC(type, tester);
+        Luhn(tester);
+        flipDig(tester);
+        if(validCC(tester)) {
+            cout << tester << " 1" << endl;
+            countP++;
+        }
+        else {
+            cout << tester << " 0" << endl;
+            countN++;
+        }
+    }
+    cout << "invalid: " << countN << ", valid: " << countP;
+    return 0;
+}
+
+void Luhn(char card[]){
+    int final = 0;
+    //size is either 15 or 16 digits depending on when the first '\0' shows up
+    int size = (card[14] != '\0') ? 15 : 14;
+    for(int i = 1; i <= size; i++) {
+        final += ((card[size-i]-48)*(1+i%2) < 10) ? (card[size-i]-48)*(1+i%2) : (card[size-i]-48)*(1+i%2)-9;
+    }
+    final *= 9;
+    final %= 10;
+    card[size] = final + '0';
+    return;
+}
+
+void genCC(CrdCard kind, char card[]) {
+    //beginnings for cards
+    //9 = random, 8 = 3 or 7
+    int size = (kind == AmEx) ? 16 : 17;
+    char begin[5][4] = {{'3', '8', '9', '9'}, //AmEx
+        {'4','9','9','9'}, //Visa
+        {'5', (rand()%5+1)+'0','9','9'}, //MasterCard
+        {'6','0','1','1'}, //Discover
+        {'0','0','0','0'}}; //all
+        for(int i = 0; i < size-1; i++)   {
+            while(i < 4)    {
+                if(begin[kind][i] == '8')   {
+                    card[i] = (rand()%2 == 1) ? '4' : '7';
+                }
+                else if(begin[kind][i] == '9')  {
+                    card[i] = rand()%10 + '0';
+                }
+                else    {
+                    card[i] = begin[kind][i];
+                }
+                i++;
+            }
+            card[i] = rand()%10 + '0';
+        }
+        for(int i = size; i <= size; i++)    {
+            card[i] = '\0';
+        }
+        return;
+}
+
+void flipDig(char card[])    {
+    int size = (card[15] != '\0') ? 16 : 15;
+    card[rand()%size] = rand()%10 + '0';
+}
+
+bool validCC(char card[])    {
+    int final = 0;
+    int size = (card[15] != '\0') ? 15 : 14;
+    for(int i = 1; i <= size; i++) {
+        final += ((card[size-i]-48)*(1+i%2) < 10) ? (card[size-i]-48)*(1+i%2) : (card[size-i]-48)*(1+i%2)-9;
+    }
+    final *= 9;
+    final %= 10;
+    return ((final + '0') == card[size]);
+}
